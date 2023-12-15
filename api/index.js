a5f6a247a6d2bd75d62bcda241829c9082613e83
TejasPatne <tejaspatne18@gmail.com>
2023-12-15 11:24:16 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`connected to MongoDB`);
+    })
+    .catch((error) => {
+        console.log(`Something went wrong while connecting to database: ${error}`);
+    });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Server is running on port ${3000}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
