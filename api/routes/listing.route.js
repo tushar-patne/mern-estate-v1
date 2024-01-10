f3bb54050e5f891c8dc57cd700dcd8b280d92cd1
TejasPatne <tejaspatne18@gmail.com>
2024-01-10 08:50:48 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
 router.post('/update/:id', verifyUser, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
