2c043ff8d33a3d38d882a20af6440ce97097d17a
TejasPatne <tejaspatne18@gmail.com>
2024-01-11 20:24:16 +0530

    add onChange and onSubmit functionality to the search page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -178,10 +178,10 @@ export default function Profile() {
           {
             userListings.map((listing) => (
               <div key={listing._id} className='border rounded-lg flex justify-between items-center p-3 gap-4' >
-                <Link to={`/listings/${listing._id}`}>
+                <Link to={`/listing/${listing._id}`}>
                   <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain' />
                 </Link>
-                <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listings/${listing._id}`}>
+                <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
                   <p>{listing.name}</p>
                 </Link>
                 <div className='flex flex-col items-center'>
